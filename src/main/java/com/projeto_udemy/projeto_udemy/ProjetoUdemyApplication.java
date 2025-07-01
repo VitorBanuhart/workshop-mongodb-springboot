c5a7681ef25432cc6a05868c4c6dfe47f38971f1
Vitor Banuhart <vitorbanuhart123@gmail.com>
2025-07-01 10:57:50 -0300

    initial-commit

---
+++ src/main/java/com/projeto_udemy/projeto_udemy/ProjetoUdemyApplication.java
@@ -0,0 +1,13 @@
+package com.projeto_udemy.projeto_udemy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoUdemyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoUdemyApplication.class, args);
+	}
+
+}
