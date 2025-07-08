5191992a767e21b6ee1dd5fd1c022b89a60eb5c1
Vitor Banuhart <vitorbanuhart123@gmail.com>
2025-07-08 14:11:51 -0300

    consulta simples com @query

--- src/main/java/com/projeto_udemy/projeto_udemy/repository/PostRepository.java
+++ src/main/java/com/projeto_udemy/projeto_udemy/repository/PostRepository.java
@@ -3,12 +3,17 @@ package com.projeto_udemy.projeto_udemy.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.projeto_udemy.projeto_udemy.domain.Post;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String>{
+
+
+    @Query("{ 'title' : {$regex: ?0, $options: 'i' }}")
+    List<Post> findByTitle(String title);
 
     List<Post> findByTitleContainingIgnoreCase(String title);
 }
