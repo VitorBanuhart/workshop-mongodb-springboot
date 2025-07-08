25ae743a4fb1a036f4b55a24566c3bddf27a895f
Vitor Banuhart <vitorbanuhart123@gmail.com>
2025-07-08 07:54:41 -0300

    post method

--- src/main/java/com/projeto_udemy/projeto_udemy/services/UserServices.java
+++ src/main/java/com/projeto_udemy/projeto_udemy/services/UserServices.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.projeto_udemy.projeto_udemy.domain.User;
+import com.projeto_udemy.projeto_udemy.dto.UserDTO;
 import com.projeto_udemy.projeto_udemy.repository.UserRepository;
 import com.projeto_udemy.projeto_udemy.services.exception.ObjectNotFoundException;
 
@@ -23,5 +24,13 @@ public class UserServices {
     public User findById(String id) {
         Optional<User> obj = userRepository.findById(id);
         return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+    }
+
+    public User insert(User obj) {
+        return userRepository.insert(obj);
+    }
+
+    public User fromDTO(UserDTO objDto) {
+        return new User(objDto.getId(), objDto.getNome(), objDto.getEmail());
     }
 }
