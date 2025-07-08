184b679b81b92ddcac27589c3bce263d71973a5d
Vitor Banuhart <vitorbanuhart123@gmail.com>
2025-07-08 13:57:36 -0300

    pesquisa por titulo do post

--- src/main/java/com/projeto_udemy/projeto_udemy/resource/PostResource.java
+++ src/main/java/com/projeto_udemy/projeto_udemy/resource/PostResource.java
@@ -1,14 +1,18 @@
 package com.projeto_udemy.projeto_udemy.resource;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.projeto_udemy.projeto_udemy.domain.Post;
 import com.projeto_udemy.projeto_udemy.services.PostServices;
+import com.projeto_udemy.projeto_udemy.util.URL;
 
 @RestController
 @RequestMapping(value = "/post")
@@ -23,5 +27,10 @@ public class PostResource {
         return ResponseEntity.ok().body(obj);
     }
 
-    
+    @GetMapping(value = "/titlesearch")
+    public ResponseEntity<List<Post>> findByTitle (@RequestParam(value = "text", defaultValue = "") String text) {
+        text = URL.decodeParam(text);
+        List<Post> list = postServices.findByTitle(text);
+        return ResponseEntity.ok().body(list);
+    }    
 }
